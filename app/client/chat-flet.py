86b25511db8bc2f0e2b445f1339e32f64a1bc12a
masnurrm <sangmastahnm@gmail.com>
2023-06-12 09:50:53 +0700

    feat: onboarding screen

--- app/client/chat-flet.py
+++ app/client/chat-flet.py
@@ -1,8 +1,6 @@
 from chatcli import *
 
-
 import flet as ft
-
 
 TARGET_IP = os.getenv("SERVER_IP") or "127.0.0.1"
 TARGET_PORT = os.getenv("SERVER_PORT") or "8889"
@@ -33,8 +31,5 @@ def main(page):
 
 
 if __name__=='__main__':
-    if (ON_WEB=="1"):
-        ft.app(target=main,view=ft.WEB_BROWSER,port=8550)
-    else:
-        ft.app(target=main)
+    ft.app(target=main,view=ft.WEB_BROWSER,port=8550)
 
